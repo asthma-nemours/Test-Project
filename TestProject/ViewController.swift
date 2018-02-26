2060f93e1fd6c9a24c813862685eb1ce8953aa82
Patel, Sanjay <Sanjay.Patel@nemours.org>
2018-02-26 14:47:25 -0500

    initial commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Patel, Sanjay on 3/31/17.
+//  Copyright © 2017 Patel, Sanjay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Find Appointments using AppointmentService
+        // Display Appointments as per UI
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
